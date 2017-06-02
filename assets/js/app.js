8f990fde8bf04d8d909e5a0eb93e33048e909f59
Neiza Nunez <neizapf@gmail.com>
2017-06-02 18:10:59 -0500

    slide verion beta

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,51 @@
 //$()=>
 
 $(_=>{
-  alert("cargo jQuery");
-});
+
+    var slideContainer = $(".slide-container"),
+        imgContainer = $(".image-container"),
+        buttonPrevious = $(".btn-previous"),
+        buttonNext = $(".btn-next");
+        $(".slide-container .image-container:last").insertBefore(".slide-container .image-container:first");
+        alert("si")
+        slideContainer.css("margin-left", "-"+ 100 +"%");
+
+         function moverD() {
+            slideContainer.animate({
+            "margin-left":'-'+200+'%'
+         } ,700, function(){
+             $(".slide-container .image-container:first").insertAfter(".slide-container .image-container:last");
+             slideContainer.css('margin-left', '-'+100+'%');
+         });
+     }
+
+     function moverI() {
+         slideContainer.animate({
+             "margin-left":0
+         } ,700, function(){
+             $(".slide-container .image-container:last").insertBefore(".slide-container .image-container:first");
+             slideContainer.css('margin-left', '-'+100+'%');
+         });
+   }
+     function autoplay() {
+       interval = setInterval(function(){
+         moverD();
+       }, 10000);
+    }
+
+    buttonNext.on('click',function() {
+       moverD();
+       clearInterval(interval);
+       autoplay();
+   });
+
+   buttonPrevious.on('click',function() {
+       moverI();
+       clearInterval(interval);
+       autoplay();
+   });
+
+
+   autoplay();
+
+ });
